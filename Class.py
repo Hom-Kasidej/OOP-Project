38a9bca237de015daf03b8c94f649d31a6f646ba
Kasidej Putthawong <tobe132546@gmail.com>
2023-03-20 21:58:04 +0700

    add get-fucntion in Class

--- Class.py
+++ Class.py
@@ -10,11 +10,10 @@ class Account:
     def get_accounts(self):
         return self.__accounts
 
-    def check_account():
+    def check_account(self):
         pass
 
 class User:
-
     def __init__(self, name, profile_image, gender, birth_date, info, username, password):
         self._name = name
         self._profile_image = profile_image
@@ -45,14 +44,21 @@ class User:
     def get_password(self):
         return self._password
         
-    def view_car():
+    def view_car(self):
         pass
 
-    def search_car():
+    def search_car(self):
         pass
 
-    def search_cartype():
+    def search_cartype(self):
         pass
+
+    def cancel_payment(self):
+        pass
+
+    def cancel_rent(self):
+        pass
+
 
 class Dealer(User):
     
@@ -63,53 +69,68 @@ class Dealer(User):
         self.__respond_time = respond_time
         self.__car_list = []
 
+    def get_accept_rate(self):
+        return self.__accept_rate
 
-        def create_car():
-            pass
+    def get_respond_rate(self):
+        return self.__respond_rate
 
-        def add_to_carcatalog():
-            pass
+    def get_respond_time(self):
+        return self.__respond_time
 
-        def modify_car():
-            pass
+    def get_car_list(self):
+        return self.__car_list
 
-        def remove_car(self):
-            pass
 
-        def deleted_car():
-            pass
+    def create_car(self):
+        pass
+
+    def add_to_carcatalog(self):
+        pass
+
+    def modify_car(self):
+        pass
+
+    def remove_car(self):
+        pass
+
+    def deleted_car(self):
+        pass
+
 
 class Renter(User):
 
     def __init__(self, name, profile_image, gender, birth_date, info, username, password):
         super().__init__(name, profile_image, gender, birth_date, info, username, password) 
-        self.__rent_list = []
         self.__success_list = []
         self.__canceled_list = []
         self.__incomplete_list = []
 
-    def cancel_payment():
+    def get_success_list(self):
+        return self.__success_list
+
+    def get_canceled_list(self):
+        return self.__canceled_list
+
+    def get_incomplete_list(self):
+        return self.__incomplete_list
+        
+    def add_to_success_list(self):
         pass
 
-    def cancel_rent():
+    def add_to_canceled_list(self):
         pass
 
-    def add_to_success_list():
+    def add_to_incomplete_list(self):
         pass
 
-    def add_to_canceled_list():
-        pass
-
-    def add_to_incomplete_list():
-        pass
-
-    def del_in_success_list():
+    def del_in_success_list(self):
         pass
     
-    def del_in_canceled_list():
+    def del_in_canceled_list(self):
         pass
     
-    def del_in_incomplete_list():
+    def del_in_incomplete_list(self):
         pass
 
     def make_rent(self,rlocation,car,check_in_date,check_out_date):
@@ -132,13 +153,16 @@ class CarType:
         self.add_to_car_catalog("Utility")
         self.add_to_car_catalog("Other")
 
+    def get_car_catalogs(self):
+        return self.__car_catalogs
+
     def add_to_car_catalog(self,car_catalog):
         self.__car_catalogs.append(car_catalog)
     
-    def search_car():
+    def search_car(self):
         pass
 
-    def search_cartype():
+    def search_cartype(self):
         pass
     
 
@@ -150,6 +174,19 @@ class CarCatalog:
         self.__type_image = type_image
         self.__car_list = []
 
+    def get_name(self):
+        return self.__name
+
+    def get_type_info(self):
+        return self.__type_info
+
+    def get_type_image(self):
+        return self.__type_image
+
+    def get_car_list(self):
+        return self.__car_list
+
+
     def add_to_carlist(self,car):
         self.__car_list.append(car)
 
@@ -160,7 +197,7 @@ class CarCatalog:
                 return_car_list.append(car)
         return car
 
-    def search_car():
+    def search_car(self):
         pass
 
 
@@ -183,6 +220,51 @@ class Car:
         self.__carstatus = carstatus
         self.__review = review
 
+    def get_brand(self):
+        return self.__brand
+
+    def get_release_year(self):
+        return self.__release_year
+
+    def get_seats(self):
+        return self.__seats
+
+    def get_doors(self):
+        return self.__doors
+
+    def get_gear_type(self):
+        return self.__gear_type
+
+    def get_fuel_type(self):
+        return self.__fuel_type
+
+    def get_distance(self):
+        return self.__distance
+
+    def get_gps_type(self):
+        return self.__gps_type
+
+    def get_color(self):
+        return self.__color
+
+    def get_features(self):
+        return self.__features
+
+    def get_info(self):
+        return self.__info
+
+    def get_images(self):
+        return self.__images
+
+    def get_price(self):
+        return self.__price
+
+    def get_carstatus(self):
+        return self.__carstatus
+
+    def get_review(self):
+        return self.__review
+
     def check_status(self,start_date,end_date):
         for rent in self.__carstatus.get_rent_list():
             if(rent.get_check_in_date() > start_date or rent.get_check_out_date < end_date):
@@ -199,6 +281,24 @@ class Review:
         self.__minute = minute
         self.__renter = renter
 
+    def get_star(self):
+        return self.__star
+
+    def get_date(self):
+        return self.__date
+
+    def get_info(self):
+        return self.__info
+
+    def get_hour(self):
+        return self.__hour
+
+    def get_minute(self):
+        return self.__minute
+
+    def get_renter(self):
+        return self.__renter
+
 class CarStatus:
     
     def __init__(self):
@@ -210,7 +310,7 @@ class CarStatus:
     def update_carstatus(self,rent):
         pass
     
-    def check_status():
+    def check_status(self):
         pass
 
 class Rent:
@@ -230,17 +330,37 @@ class Rent:
 
     def get_check_out_date(self):
         return self.__check_out_date
+    
+    def get_location(self):
+        return self.__location
+
+    def get_rent_car(self):
+        return self.__rent_car
+
+    def get_rent_no(self):
+        return self.__rent_no
+
+    def get_rent_status(self):
+        return self.__rent_status
+
+    def get_payment(self):
+        return self.__payment
+
+    def get_receipt(self):
+        return self.__receipt
+
 
     def create_payment(self,amount,date,type):
         pass
 
-    def create_receipt():
+    def create_location(self):
         pass
 
-    def update_rent_status():
+    def create_receipt(self):
         pass
 
-
+    def update_rent_status(self):
+        pass
 
 class Receipt:
     
@@ -249,11 +369,27 @@ class Receipt:
         self.__rent = rent
         self.__payment = payment
 
+    def get_receipt_no(self):
+        return self.__receipt_no
+
+    def get_rent(self):
+        return self.__rent
+
+    def get_payment(self):
+        return self.__payment
+
 class Location:
 
     def __init__(self, car_receive, car_return):
         self.__car_receive = car_receive
         self.__car_return = car_return
+
+    def get_car_receive(self):
+        return self.__car_receive
+
+    def get_car_return(self):
+        return self.__car_return
+
 
 class Payment:
     
@@ -264,7 +400,22 @@ class Payment:
         self._payment_no = payment_no
         self._rent = rent
 
-    def update_payment_status():
+    def get_amount(self):
+        return self._amount
+
+    def get_date(self):
+        return self._date
+
+    def get_payment_status(self):
+        return self._payment_status
+
+    def get_payment_no(self):
+        return self._payment_no
+
+    def get_rent(self):
+        return self._rent
+
+    def update_payment_status(self):
         pass
 
 
@@ -273,6 +424,9 @@ class CashPayment(Payment):
     def __init__(self, cash_type, amount, date, payment_status, payment_no,  rent_Rent, receipt):
         super().__init__(amount, date, payment_status, payment_no,  rent_Rent, receipt)
         self.__cash_type = cash_type
+
+    def get_cash_type(self):
+        return self.__cash_type
 
 class CreditCardPayment(Payment):
 
@@ -283,6 +437,22 @@ class CreditCardPayment(Payment):
         self.__card_number = card_number
         self.__card_CVC = card_CVC
         self.__card_exp = card_exp
+
+    def get_creditcard_type(self):
+        return self.__creditcard_type
+
+    def get_card_name(self):
+        return self.__card_name
+
+    def get_card_number(self):
+        return self.__card_number
+
+    def get_card_CVC(self):
+        return self.__card_CVC
+
+    def get_card_exp(self):
+        return self.__card_exp
+
 
 class CarBrand(Enum):
     Aston_martin = 1
@@ -354,3 +524,9 @@ class GPSType(Enum):
 class Gender(Enum):
     Male = 1
     Female = 2
+    Others = 3
+
+class Status(Enum):
+    Canceled = 1
+    Pending = 2
+    Success = 3
