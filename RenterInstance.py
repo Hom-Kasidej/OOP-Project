a37d1bbe56c0ccca13827c8ad658a516b9eb6d85
Kasidej Putthawong <62129944+Hom-Kasidej@users.noreply.github.com>
2023-04-03 16:54:59 +0700

    add user_ID

--- RenterInstance.py
+++ RenterInstance.py
@@ -8,7 +8,8 @@ renter_info_dict = {
         "birth_date" : "28/02/1987",
         "info" : "Good guy",
         "username" : "Sompong",
-        "password" : "ripperisthedog"
+        "password" : "ripperisthedog",
+        "user_ID" : ""
     },
     "Renter2" : {
         "name" : "Somsej Eatgrass",
@@ -17,7 +18,8 @@ renter_info_dict = {
         "birth_date" : "01/01/1999",
         "info" : "Capybaraaa",
         "username" : "GrossGrass",
-        "password" : "iwantgrass555"
+        "password" : "iwantgrass555",
+        "user_ID" : ""
     },
     "Renter" : {
         "name" : "Somjett DashForward",
@@ -26,7 +28,8 @@ renter_info_dict = {
         "birth_date" : "09/02/2000",
         "info" : "Get out of my way!",
         "username" : "Jett",
-        "password" : "333Jettiseasytowin"
+        "password" : "333Jettiseasytowin",
+        "user_ID" : ""
     },
      "Renter4" : {
         "name" : "Gekko keeact",
@@ -35,7 +38,8 @@ renter_info_dict = {
         "birth_date" : "21/07/2002",
         "info" : "Let's go my buddy",
         "username" : "Anonymous",
-        "password" : "OhhoMyBuddy"
+        "password" : "OhhoMyBuddy",
+        "user_ID" : ""
     },
     "Renter5" : {
         "name" : "MrBeast Latkrabang",
@@ -44,7 +48,8 @@ renter_info_dict = {
         "birth_date" : "11/11/1970",
         "info" : "Who want my money!!",
         "username" : "Beast",
-        "password" : "1234567890"
+        "password" : "1234567890",
+        "user_ID" : ""
     },
     "Renter6" : {
         "name" : "RestWith OOP",
@@ -53,7 +58,8 @@ renter_info_dict = {
         "birth_date" : "19/03/2003",
         "info" : "OOP Project is waiting for you",
         "username" : "OOPInwZa",
-        "password" : "KhorGradeANoiKrub55"
+        "password" : "KhorGradeANoiKrub55",
+        "user_ID" : ""
     },
     "Renter7" : {
         "name" : "Somying butchai",
@@ -62,7 +68,8 @@ renter_info_dict = {
         "birth_date" : "20/09/1997",
         "info" : "I want cheap price",
         "username" : "Somying",
-        "password" : "yinglowcost"
+        "password" : "yinglowcost",
+        "user_ID" : ""
     },
     "Renter8" : {
         "name" : "Somrew Lookmo",
@@ -71,7 +78,8 @@ renter_info_dict = {
         "birth_date" : "05/05/1955",
         "info" : "Parents give me money :)",
         "username" : "Want bullet?",
-        "password" : "666satan666"
+        "password" : "666satan666",
+        "user_ID" : ""
     },
     "Renter9" : {
         "name" : "Respect Thailand",
@@ -80,7 +88,8 @@ renter_info_dict = {
         "birth_date" : "20/02/1969",
         "info" : "Long Live King",
         "username" : "LoveKing",
-        "password" : "999KingKing"
+        "password" : "999KingKing",
+        "user_ID" : ""
     },
      "Renter10" : {
         "name" : "Frog obob",
@@ -89,7 +98,8 @@ renter_info_dict = {
         "birth_date" : "27/12/1975",
         "info" : "Born in R9",
         "username" : "King9",
-        "password" : "loveKingThailand"
+        "password" : "loveKingThailand",
+        "user_ID" : ""
     }
 }
 
