dbe00f794f04f3d847cb8274ca7f35bda300569a
NTPNTPNTP <natapat.raungput.2547@gmail.com>
2023-04-04 15:07:42 +0700

    add ID in dealer, car class

--- scriptRemoveCar.py
+++ scriptRemoveCar.py
@@ -4,7 +4,7 @@ import CarInstance as cars
 
 ###########################################################
 
-carcatalog = C.CarCatalog()
+catalog = C.System()
 
 dealer1 = dealers.dealer_instance_list[0]
 dealer2 = dealers.dealer_instance_list[1]
@@ -12,25 +12,25 @@ dealer2 = dealers.dealer_instance_list[1]
 i = 0
 for car in cars.Car_instance_list:
     if i != 12:
-        carcatalog.add_to_car_list(car)
+        catalog.add_car(car)
     i += 1
 
 ###########################################################
 
 def print_all():
     print("Catalog : ", end="")
-    for car in carcatalog.get_car_list():
+    for car in catalog.get_car_list():
         print(f"{car.get_car_ID()}({car.get_dealer_ID()})", end=" ")
     print()
 
     print("Dealer 1's catalog : ", end="")
-    for car in carcatalog.get_car_list():
+    for car in catalog.get_car_list():
         if car.get_dealer_ID() == dealer1.get_ID():
             print(f"{car.get_car_ID()}", end=" ")
     print()
 
     print("Dealer 2's catalog : ", end="")
-    for car in carcatalog.get_car_list():
+    for car in catalog.get_car_list():
         if car.get_dealer_ID() == dealer2.get_ID():
             print(f"{car.get_car_ID()}", end=" ")
     print()
@@ -39,11 +39,11 @@ def print_all():
 
 print_all()
 
-dealer1.remove_car(carcatalog, carcatalog.get_car_list()[4])
+dealer1.remove_car(catalog, catalog.get_car_list()[4])
 print_all()
 
-dealer2.remove_car(carcatalog, carcatalog.get_car_list()[0])
+dealer2.remove_car(catalog, catalog.get_car_list()[0])
 print_all()
 
-dealer2.post_car(carcatalog, cars.car_info_dict["Car13"])
+dealer2.post_car(catalog, cars.car_info_dict["Car13"])
 print_all()
