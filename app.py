a8e37889fa51cfe5bf91821574968aa1216a7ab1
Katapkoon <katapat80@gmail.com>
2023-04-18 14:00:07 +0700

    tkinter add search car template

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
 from tkinter import *
+from tkinter import ttk
 import requests
 import tkinter.font as tkFont
+
 
 class MainApp(Tk):
     def __init__(self):
         Tk.__init__(self) 
         self.container = Frame(self)
-        self.container.pack(fill = "both", expand = True)
+        self.container.pack(fill=BOTH,expand=YES)
+        self.container.config()
+        
         self.frame = StartPage(self.container,self)
-        self.frame.pack(fill='both')
+        self.frame.pack(fill=BOTH,expand=YES)
 
     def change_frame(self,frame):
         self.frame.destroy()
         self.frame = frame(self.container,self)
-        self.frame.pack(fill='both')
+        self.frame.pack(fill=BOTH,expand=YES)
+class ScrollableFrame(Frame):
+    def __init__(self, container, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
 
+        container.bind_all("<MouseWheel>", self._on_mousewheel)
+        self.canvas =Canvas(container)
+        self.scrollbar = Scrollbar(container, orient="vertical", command=self.canvas.yview)
+        self.scrollable_frame = Frame(self.canvas)
+
+        self.scrollable_frame.bind(
+            "<Configure>",
+            lambda e: self.canvas.configure(
+                scrollregion=self.canvas.bbox("all")
+            )
+        )
+
+        self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="n")
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.canvas.pack(side='left',fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
+
+    def _on_mousewheel(self, event):
+        self.canvas.yview_scroll(int(-1*(event.delta/120)), "units")
 class StartPage(Frame):
     api_endpoint = "http://127.0.0.1:8000/"
     logout_endpoint = "http://127.0.0.1:8000/logout"
@@ -22,22 +50,113 @@ class StartPage(Frame):
     def __init__(self,parent,controller):
         Frame.__init__(self,parent)
         self.response = requests.get(self.api_endpoint)
+
+        top_bar_frame = Frame(self,bg='pink')
+        top_bar_frame.pack(side=TOP,fill=X)
         if self.response.ok:
-            self.label = Label(self,text="This is the starting page. Welcome! " + self.response.json()['data'])
-            self.label.pack()
-            self.login_button = Button(self,text="Log in",command=lambda:controller.change_frame(LoginPage))
-            self.login_button.pack()
-            self.register_button = Button(self,text="Register",command=lambda:controller.change_frame(RegisterPage))
-            self.register_button.pack()
+            #top bar frame elements
+            self.label = Label(top_bar_frame,text="This is the starting page. Welcome! " + self.response.json()['data'])
+            self.label.pack(side=LEFT,anchor=W)
+            self.login_button = Button(top_bar_frame,text="Log in",command=lambda:controller.change_frame(LoginPage))
+            self.login_button.pack(side=RIGHT)
+            self.register_button = Button(top_bar_frame,text="Register",command=lambda:controller.change_frame(RegisterPage))
+            self.register_button.pack(side=RIGHT)
+            '''
+            self.canvas = Canvas(self)
+            self.scrollbar = Scrollbar(self,orient=VERTICAL,command=self.canvas.yview)
+            self.scrollable_frame = Frame(self.canvas)
+            self.scrollable_frame.bind("<Configure>",lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
+            self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+            self.canvas.configure(yscrollcommand=self.scrollbar.set)
+            self.canvas.pack(side="left", fill="both", expand=True)
+            self.scrollbar.pack(side="right", fill="y")
+            '''
+            
+            #scrollable frame
+            
+            self.main_frame = Frame(self,bg='gray')
+            self.main_frame.pack(fill=BOTH,expand=YES)
+            
+            '''
+            for i in range(50):
+                Label(self.main_frame.scrollable_frame, text=f"Sample scrolling label{i}").pack()
+
+            '''  
+            drivemate_logo = Label(self.main_frame,text = "DRIVEMATE",font = 100,bg ="yellow",fg ="red")
+            drivemate_logo.pack()
+
+            self.searchbar_frame = Frame(self.main_frame,bg='cyan')
+            self.searchbar_frame.pack(side=TOP,fill=X,expand=YES)
+
+            myLabel2 = Label(self.searchbar_frame,text = "จาก :",font = 50)
+            myLabel2.pack(side=LEFT,expand=YES,fill=X)
+            cbo_day_pickup = ttk.Combobox(self.searchbar_frame, values = list(range(1,32)), width = 3, state="readonly")
+            cbo_day_pickup.current(0)
+            cbo_day_pickup.pack(side=LEFT,expand=YES,fill=X)
+            cbo_month_pickup = ttk.Combobox(self.searchbar_frame, values = list(range(1,13)), width = 4,state = "readonly")
+            cbo_month_pickup.current(1)
+            cbo_month_pickup.pack(side=LEFT,expand=YES,fill=X)
+            cbo_year_pickup = ttk.Combobox(self.searchbar_frame, values = list(range(2023,2100)), width = 5)
+            cbo_year_pickup.current(1)
+            cbo_year_pickup.pack(side=LEFT,expand=YES,fill=X)
+
+            myLabel3 = Label(self.searchbar_frame,text = "เวลา :",font = 50)
+            myLabel3.pack(side=LEFT,expand=YES,fill=X)
+            cbo_hour_pickup = ttk.Combobox(self.searchbar_frame, values = list(range(1,25)),width = 2,state = "readonly")
+            cbo_hour_pickup.current(1)
+            cbo_hour_pickup.pack(side=LEFT,expand=YES,fill=X)
+            cbo_minute_pickup = ttk.Combobox(self.searchbar_frame, values = list(range(0,61)),width = 2,state = "readonly")
+            cbo_minute_pickup.current(1)
+            cbo_minute_pickup.pack(side=LEFT,expand=YES,fill=X)
+
+           
+
+            myLabel4 = Label(self.searchbar_frame,text = "จนกระทั่ง :",font = 50)
+            myLabel4.pack(side=LEFT,expand=YES,fill=X)
+            cbo_day_return = ttk.Combobox(self.searchbar_frame, values = list(range(1,32)), width = 3, state="readonly")
+            cbo_day_return.current(0)
+            cbo_day_return.pack(side=LEFT,expand=YES,fill=X)
+    
+            cbo_month_return = ttk.Combobox(self.searchbar_frame, values = list(range(1,13)), width = 4,state = "readonly")
+            cbo_month_return.current(1)
+            cbo_month_return.pack(side=LEFT,expand=YES,fill=X)
+    
+            cbo_year_return = ttk.Combobox(self.searchbar_frame, values = list(range(2023,2100)), width = 5)
+            cbo_year_return.current(1)
+            cbo_year_return.pack(side=LEFT,expand=YES,fill=X)
+            myLabel5 = Label(self.searchbar_frame,text = "เวลา :",font = 50)
+            myLabel5.pack(side=LEFT,expand=YES,fill=X)
+            cbo_hour_return = ttk.Combobox(self.searchbar_frame, values = list(range(1,25)),width = 2,state = "readonly")
+            cbo_hour_return.current(1)
+            cbo_hour_return.pack(side=LEFT,expand=YES,fill=X)
+    
+            cbo_minute_return = ttk.Combobox(self.searchbar_frame, values = list(range(0,61)),width = 2,state = "readonly")
+            cbo_minute_return.current(1)
+            cbo_minute_return.pack(side=LEFT,expand=YES,fill=X)
+
+            provinces = {"Amnat Charoen", "Ang Thong", "Bangkok", "Bueng Kan", "Buri Ram", "Chachoengsao", "Chai Nat", "Chaiyaphum", "Chanthaburi", "Chiang Mai", "Chiang Rai", "Chon Buri", "Chumphon", "Kalasin", "Kamphaeng Phet", "Kanchanaburi", "Khon Kaen", "Krabi", "Lampang", "Lamphun", "Loei", "Lopburi", "Mae Hong Son", "Maha Sarakham", "Mukdahan", "Nakhon Nayok", "Nakhon Pathom", "Nakhon Phanom", "Nakhon Ratchasima", "Nakhon Sawan", "Nakhon Si Thammarat", "Nan", "Narathiwat", "Nong Bua Lamphu", "Nong Khai", "Nonthaburi", "Pathum Thani", "Pattani", "Phang Nga", "Phatthalung", "Phayao", "Phetchabun", "Phetchaburi", "Phichit", "Phitsanulok", "Phra Nakhon Si Ayutthaya", "Phrae", "Phuket", "Prachinburi", "Prachuap Khiri Khan", "Ranong", "Ratchaburi", "Rayong", "Roi Et", "Sa Kaeo", "Sakon Nakhon", "Samut Prakan", "Samut Sakhon", "Samut Songkhram", "Saraburi", "Satun", "Sing Buri", "Sisaket", "Songkhla", "Sukhothai", "Suphan Buri", "Surat Thani", "Surin", "Tak", "Trang", "Trat", "Ubon Ratchathani", "Udon Thani", "Uthai Thani", "Uttaradit", "Yala", "Yasothon"}
+            tv_string = StringVar()
+
+            selectedOpt = StringVar()
+            selectedOpt.set('Bangkok')
+    
+            om = OptionMenu(self.searchbar_frame, selectedOpt, *provinces)
+            om.config(width = 20)
+            om.pack(side=LEFT,expand=YES,fill=X)
+    
+            btn = Button(self.searchbar_frame, text ="Search")
+            btn.pack(side=LEFT,expand=YES,fill=X)
+            btn.bind("<Button-1>")
+            
         if self.response.json()['data'] != 'Guest':
             self.login_button.pack_forget()
             self.register_button.pack_forget()
-            self.logout_button = Button(self,text="Log out",command=self.logout_click)
-            self.logout_button.pack()
+            self.logout_button = Button(top_bar_frame,text="Log out",command=self.logout_click)
+            self.logout_button.pack(side=RIGHT)
     def logout_click(self):
         new_response = requests.get(self.logout_endpoint)
-        self.login_button.pack()
-        self.register_button.pack()
+        self.login_button.pack(side=RIGHT)
+        self.register_button.pack(side=RIGHT)
         self.logout_button.pack_forget()
         self.label.config(text="This is the starting page. Welcome! " +'Guest')
 
